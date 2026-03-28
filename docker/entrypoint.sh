404d28b74ba3f952686bc1c5e50ca97112a0dc54
Mike Church <church.mike@protonmail.com>
2026-03-28 12:40:44 +0700

    docker: set mcp-proxy arg instead of tools

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
-llama_opts=('')
+extra_llama_opts=('')
 
 if [[ $FUNCTION == "compute" ]]; then
   echo Starting rpc-server worker node...
   if [[ $USE_CACHE == "true" ]]; then
-    llama_opts+=("-c")
+    extra_llama_opts+=("-c")
   fi
-  rpc-server --host ${RPC_HOST} --port ${RPC_PORT} --threads ${NUM_THREADS} ${llama_opts[@]}
+  exec rpc-server \
+                  --host ${RPC_HOST} \
+                  --port ${RPC_PORT} \
+                  --threads ${NUM_THREADS} \
+                  ${extra_llama_opts[@]}
 elif [[ $FUNCTION == "frontend" ]]; then
   echo Starting llama-server frontend node...
   rpc_list=$(printf "%s:50052," ${RPC_HOST//,/ })
   rpc_list="${rpc_list%,}"  # Trim trailing comma
-  llama-server --host ${WEB_HOST} \
-	       --port ${WEB_PORT} \
-	       --model ${MODEL_PATH} \
-	       --threads ${NUM_THREADS}\
-	       --rpc ${rpc_list} \
-	       --tools ${MCP_URL} \
-	       ${llama_opts[@]}
+
+  if [[ -n ${MODEL_CHAT_TEMPLATE} ]]; then
+    extra_llama_opts+=("--chat-template ${MODEL_CHAT_TEMPLATE}")
+  fi
+  exec llama-server \
+                  --host ${WEB_HOST} \
+                  --port ${WEB_PORT} \
+                  --model ${MODEL_PATH} \
+                  --threads ${NUM_THREADS}\
+                  --rpc ${rpc_list} \
+                  --webui-mcp-proxy ${MCP_URL} \
+                  ${extra_llama_opts[@]}
 fi
